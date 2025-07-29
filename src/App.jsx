844e7501795083616afbde842c76b3120f3db509
VASANTH0999 <mvrreddy1374@gmail.com>
2025-07-29 12:08:35 +0530

    reactci committed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from '.react.svg'
+import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
@@ -8,8 +8,8 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo" alt="React logo" />
+        <a href="https://vite.dev" target="_blank">
+          <img src={reactLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
